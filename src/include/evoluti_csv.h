4cf3ccea875fe8c3f5eeeb8a2a82ab7f60bbd1f3
Bryan Morfe <bryanmorfe@gmail.com>
2019-12-19 17:40:47 -0500

    Removed EV Module that will not be used

--- src/include/evoluti_csv.h
+++ src/include/evoluti_csv.h
@@ -1,12 +1,6 @@
 #ifndef _E_EVOLUTI_CSV_H_
 #define _E_EVOLUTI_CSV_H_
 
-#include "ev_module.h"
-
-// EV_MODULE_NAME("Evoluti CSV for C++");
-// EV_MODULE_VERSION(0, 0, 1);
-// EV_MODULE_ID(0x45, 0x56, 0x00, 0x00, 0x00, 0x00, 0x00, 0x04);
-
 #include "csv_container.h"
 #include "csv_reader.h"
 #include "csv_row.h"
